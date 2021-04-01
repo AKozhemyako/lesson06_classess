7a7de19db3a1985a7971f145cd69219d5b1d635d
AKozhemyako <admin_tp@ukr.net>
2021-04-01 16:47:06 +0300

    add class Car and Point

--- Lesson08/vehicle.py
+++ Lesson08/vehicle.py
@@ -1,3 +1,9 @@
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
 class Vehicle:
     def __int__(self, point, price, speed, year):
         self.point = point
@@ -5,8 +11,23 @@ class Vehicle:
         self.speed = speed
         self.year = year
 
+
 class Plane(Vehicle):
     def __init__(self, point, price, speed, year, height, count_pass):
-        super(Plane, self).__init__()
+        super().__init__(point, price, speed, year)
         self.height = height
         self.count_pass = count_pass
+
+
+class Ship(Vehicle):
+    def __int__(self, point, price, speed, year, port, count_pass):
+        super().__int__(point, price, speed, year)
+        self.port = port
+        self.count_pass = count_pass
+
+
+class Car(Vehicle):
+    def __int__(self, point, price, speed, year):
+        super().__int__(point, price, speed, year)
+
+
