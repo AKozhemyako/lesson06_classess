df03c7a6781a091f80d316419b8cd16ee62f0a0d
AKozhemyako <admin_tp@ukr.net>
2021-04-01 19:07:29 +0300

    add Passenger class

--- Lesson08/main.py
+++ Lesson08/main.py
@@ -1,10 +1,11 @@
-
 from Lesson08.vehicle import *
 
 if __name__ == '__main__':
     vehicle_list = [Plane(Point(1, 2), 10000, 200, 1990, 20, 15),
-                    Plane(Point(2, 3), 10000, 200, 1990, 20, 25)
-                    Ship(Point(3, 4), 10000, 200, 2000, "Odessa", 150)
-                    Car(Point(4, 5), 5000, 200, 2001),
-                    Vehicle(Point(0, 0), 0, 0, 0)]
+                    Plane(Point(2, 3), 10000, 200, 1990, 20, 25),
+                    Ship(Point(4, 3), 20000, 200, 2000, "Odessa", 150),
+                    Car(Point(4, 5), 5000, 200, 2001)
+                    ]
     print(vehicle_list)
+    for vehicle in vehicle_list:
+        vehicle.show_image()
--- Lesson08/vehicle.py
+++ Lesson08/vehicle.py
@@ -1,25 +1,46 @@
-from Lesson08.coordinate import Point
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+import abc
+from abc import ABC
 
+from Lesson08.coordinate import *
 
-class Vehicle:
-    def __int__(self, point:Point, price, speed, year):
+class Passenger(metaclass=abc.ABCMeta):
+    def __init__(self, count_pass):
+        self.count_pass = count_pass
+
+    @property
+    def count_pass(self):
+        return self.__count_pass
+
+    @count_pass.setter
+    def count_pass(self, value):
+        if value > 0:
+            self.__count_pass = value
+        else:
+            self.__count_pass = 0
+
+class Vehicle(metaclass=abc.ABCMeta):
+    def __int__(self, point: Point, price: float, speed: float, year: int):
         self.point = point
         self.price = price
         self.speed = speed
         self.year = year
+
     def __str__(self) -> str:
         return f"{self.point}, {self.price}, {self.speed}, {self.year}"
 
-
-
-
-
-
-
-
-
-
-
+    @abc.abstractmethod
+    def show_image(self):pass
 
 
 
@@ -30,10 +51,13 @@ class Plane(Vehicle):
         self.count_pass = count_pass
 
     def __str__(self) -> str:
-        return f"{super().__str__()}. {self.height}, {self.count_pass}"
+        return f"{super().__str__()},{self.height},{self.count_pass}"
 
     def __repr__(self) -> str:
-        return f"Plane: {self.__str__()}"
+        return f"\nPlane: {self.__str__()}"
+
+    def show_image(self):
+        print("Plane")
 
 
 class Ship(Vehicle):
@@ -41,17 +65,24 @@ class Ship(Vehicle):
         super().__int__(point, price, speed, year)
         self.port = port
         self.count_pass = count_pass
+
     def __str__(self) -> str:
         return f"{super().__str__()}. {self.port}, {self.count_pass}"
 
     def __repr__(self) -> str:
-        return f"Ship: {self.__str__()}"
+        return f"\nShip: {self.__str__()}"
+
+    def show_image(self):
+        print("Ship")
 
 class Car(Vehicle):
     def __int__(self, point, price, speed, year):
         super().__int__(point, price, speed, year)
 
-
     def __repr__(self) -> str:
-        return f"Car: {self.__str__()}"
+        return f"\nCar: {self.__str__()}"
+
+    def show_image(self):
+        pass
+
 
