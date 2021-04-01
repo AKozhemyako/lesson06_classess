f33844425cdd37c1081bbe510f65a027cec90068
AKozhemyako <admin_tp@ukr.net>
2021-04-01 17:13:43 +0300

    print class plane

--- Lesson08/main.py
+++ Lesson08/main.py
@@ -1,2 +1,5 @@
+from Lesson08.vehicle import Plane, Point
+
 if __name__ == '__main__':
-    vehicle_list
+    vehicle_list = [Plane(Point(1, 2), 10000, 200, 1990, 20, 15)]
+    print(vehicle_list[0])
--- Lesson08/vehicle.py
+++ Lesson08/vehicle.py
@@ -1,15 +1,14 @@
-class Point:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+from Lesson08.coordinate import Point
 
 
 class Vehicle:
-    def __int__(self, point, price, speed, year):
+    def __int__(self, point:Point, price, speed, year):
         self.point = point
         self.price = price
         self.speed = speed
         self.year = year
+    def __str__(self) -> str:
+        return f"{self.point}, {self.price}, {self.speed}, {self.year}"
 
 
 class Plane(Vehicle):
@@ -17,6 +16,9 @@ class Plane(Vehicle):
         super().__init__(point, price, speed, year)
         self.height = height
         self.count_pass = count_pass
+
+    def __str__(self) -> str:
+        return f"{super().__str__()}. {self.height}, {self.count_pass}"
 
 
 class Ship(Vehicle):
